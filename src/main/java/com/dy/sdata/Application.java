ffe17406a5ef287e694c5cf0a762561b1a012b33
dongy <my@dy.com>
2017-10-10 11:40:38 +0200

    Delete entity

--- src/main/java/com/dy/sdata/Application.java
+++ src/main/java/com/dy/sdata/Application.java
@@ -14,25 +14,37 @@ public class Application {
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
 		
+		// 1. Use BookService to create an entity
 //		BookService service = context.getBean(BookService.class);
 //		Book book = new Book("First book", new Date(), 33, new BigDecimal("26.60"));
 //		service.save(book);
 		
+		
 		BookRepository bookRepository = context.getBean(BookRepository.class);
 		
+		// 2. Use repository to find, create, modify, delete entity
 		// Book book = bookRepository.findOne(1L);
 		// List<Book> books = bookRepository.findAll();
 		
-		List<Book> books = BookUtil.create(5);		
-		bookRepository.save(books);
-
-		books = bookRepository.findAll();
-		for(Book book1:books) {
-			if(book1.getBookId().equals(new Long("10"))){
-				book1.setTitle("book 10th");
-				book1.setPageCount(100000);
-			}
-			System.out.println(book1);
+//		List<Book> books = BookUtil.create(5);		
+//		bookRepository.save(books);
+//
+//		books = bookRepository.findAll();
+//		for(Book book1:books) {
+//			if(book1.getBookId().equals(new Long("10"))){
+//				book1.setTitle("book 10th");
+//				book1.setPageCount(100000);
+//			}
+//			System.out.println(book1);
+//		}
+		
+		// bookRepository.delete(1L);
+//		bookRepository.delete(bookRepository.findOne(1L));
+		
+//		bookRepository.deleteAll();
+		bookRepository.deleteAllInBatch();
+		for(Book book:bookRepository.findAll()) {
+			System.out.println(book);
 		}
 		
 	}
