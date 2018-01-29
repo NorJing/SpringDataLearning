61d2de9ccf3769936f125cc1be80f096c036635c
Percy <dy@dy.com>
2018-01-29 08:08:22 +0100

    Use MongoDbFactory

--- src/main/java/com/dy/sdata/Application.java
+++ src/main/java/com/dy/sdata/Application.java
@@ -1,20 +1,20 @@
 package com.dy.sdata;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.mongodb.MongoDbFactory;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
-import com.mongodb.MongoClient;
 
 public class Application {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
-		MongoClient client = (MongoClient)context.getBean("mongoClient");
-		DB db = client.getDB("sandbox");
+		MongoDbFactory factory = context.getBean(MongoDbFactory.class);
+		DB db = factory.getLegacyDb();
 		DBCollection collection = db.getCollection("book");
-		collection.insert(new BasicDBObject().append("title", "Casablanca beach!"));
+		collection.insert(new BasicDBObject().append("title", "Oslo snow!"));
 	}
 	
 }
